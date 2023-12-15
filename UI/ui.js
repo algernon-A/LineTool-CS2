50f93736354638ed5efa7a786bf61e68ba85be85
algernon-A <aeneas@aloku.net>
2023-12-15 15:14:47 +1100

    Add licensing.

--- UI/ui.js
+++ UI/ui.js
@@ -1,5 +1,7 @@
 // <copyright file="ui.js" company="algernon (K. Algernon A. Sheppard)">
 // Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the Apache Licence, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// See LICENSE.txt file in the project root for full license information.
 // </copyright>
 
 // Function to adjust spacing.
