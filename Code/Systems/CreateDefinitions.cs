b21bc84022b0a9fd2e16dcc72e922124981974e9
algernon <aeneas@aloku.net>
2025-01-09 16:30:02 +1100

    StyleCop project structure.

--- Code/Systems/CreateDefinitions.cs
+++ Code/Systems/CreateDefinitions.cs
@@ -1,6 +1,13 @@
-﻿// Substantial portions derived from game code.
-// Copyright (c) Colossal Order and Paradox Interactive.
-// Not for reuse or distribution except in accordance with the Paradox Interactive End User License Agreement.
+﻿// <copyright file="CreateDefinitions.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the Apache Licence, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+/* Substantial portions derived from game code.
+ * Copyright (c) Colossal Order and Paradox Interactive.
+ * Not for reuse or distribution except in accordance with the Paradox Interactive End User License Agreement.
+ */
 
 namespace LineTool
 {
